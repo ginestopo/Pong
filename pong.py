72edbad7ac32c14b12798b423208ef528b7f55e2
ginestopo <ginestopo@gmail.com>
2021-01-24 13:13:13 +0100

    template created

--- pong.py
+++ pong.py
@@ -1,1 +1,23 @@
 #Pong by Gines Diaz
+import sys
+import pygame as py
+
+py.init()
+clock = py.time.Clock()
+
+screen_width = 1000
+screen_height = 600
+screen = py.display.set_mode((screen_width,screen_height))
+py.display.set_caption('Game')
+
+while True:
+
+    for event in py.event.get():
+        if event.type == py.QUIT:
+            py.quit()
+            sys.exit()
+
+
+    #update the window
+    py.display.flip()
+    clock.tick(60)
