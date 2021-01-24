23a6f896e397e79a5c38fadb95c4145f75d9655a
ginestopo <ginestopo@gmail.com>
2021-01-24 14:05:10 +0100

    player finish

--- pong.py
+++ pong.py
@@ -11,6 +11,23 @@ screen_height = 600
 screen = py.display.set_mode((screen_width,screen_height))
 py.display.set_caption('Game')
 
+#functions
+def ball_animation():
+        global ball_speed_x,ball_speed_y
+
+        ball.x += ball_speed_x
+        ball.y += ball_speed_y
+
+        if ball.top <= 0 or ball.bottom >= screen_height:
+            ball_speed_y *= -1
+
+        if ball.left <= 0 or ball.right >= screen_width:
+            ball_speed_x *= -1
+
+        if ball.colliderect(player) or ball.colliderect(opponent):
+            ball_speed_x *= -1
+
+
 #colors
 WHITE = (255,255,255)
 
@@ -22,6 +39,8 @@ opponent = py.Rect(screen_width-20,screen_height/2,10,70)
 bg_color = py.Color('grey12')
 light_grey = (200,200,200)
 
+#player
+player_speed = 0
 #ball parameters
 ball_speed_x = 7
 ball_speed_y = 7
@@ -32,17 +51,27 @@ while True:
         if event.type == py.QUIT:
             py.quit()
             sys.exit()
+        if event.type == py.KEYDOWN:
+            if event.key == py.K_DOWN:
+                player_speed += 7
+            if event.key == py.K_UP:
+                player_speed -= 7
+        if event.type == py.KEYUP:
+            if event.key == py.K_DOWN:
+                player_speed -= 7
+            if event.key == py.K_UP:
+                player_speed += 7
 
     screen.fill(bg_color)
 
-    ball.x += ball_speed_x
-    ball.y += ball_speed_y
+    ball_animation()
+    player.y += player_speed
 
-    if ball.top <= 0 or ball.bottom >= screen_height:
-        ball_speed_y *= -1
+    if(player.top <= 0):
+        player.y = 0
+    if(player.bottom >= screen_height):
+        player.y = screen_height - 70
 
-    if ball.left <= 0 or ball.right >= screen_width:
-        ball_speed_x *= -1
 
     py.draw.rect(screen,light_grey,player)
     py.draw.rect(screen,light_grey,opponent)
