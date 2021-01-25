8fc7dc24f23466754408b9a485a157b647ccf911
ginestopo <ginestopo@gmail.com>
2021-01-25 19:36:07 +0100

    deleted global variables. Class created

--- pong.py
+++ pong.py
@@ -12,38 +12,64 @@ screen = py.display.set_mode((screen_width,screen_height))
 py.display.set_caption('Game')
 
 #functions
-def ball_animation():
-        global ball_speed_x,ball_speed_y
+class ball(py.sprite.Sprite):
+    def __init__(self,x,y,x_size,y_size):
+        #ball parameters
+        self.ball_speed_x = 7
+        self.ball_speed_y = 7
 
-        ball.x += ball_speed_x
-        ball.y += ball_speed_y
+        py.sprite.Sprite.__init__(self)
 
-        if ball.top <= 0 or ball.bottom >= screen_height:
-            ball_speed_y *= -1
+        self.rect = py.Rect(x,y,x_size,y_size)
 
-        if ball.left <= 0 or ball.right >= screen_width:
-            ball_speed_x *= -1
+    def ball_update(self):
 
-        if ball.colliderect(player) or ball.colliderect(opponent):
-            ball_speed_x *= -1
+            self.rect.x += self.ball_speed_x
+            self.rect.y += self.ball_speed_y
+
+            if self.rect.top <= 0 or self.rect.bottom >= screen_height:
+                self.ball_speed_y *= -1
+
+            if self.rect.left <= 0 or self.rect.right >= screen_width:
+                self.ball_speed_x *= -1
+
+            if self.rect.colliderect(player) or self.rect.colliderect(opponent):
+                self.ball_speed_x *= -1
+
+class player(py.sprite.Sprite):
+    def __init__(self,x,y,x_size,y_size):
+        #ball parameters
+        self.player_speed = 0
+
+        py.sprite.Sprite.__init__(self)
+
+        self.rect = py.Rect(x,y,x_size,y_size)
+
+    def player_move(self,vel):
+            self.player_speed += vel
+
+    def player_update(self):
+            pos = py.mouse.get_pos()
+            self.rect.center = pos
+            '''self.rect.y += self.player_speed
+            if(self.rect.top <= 0):
+                self.rect.y = 0
+            if(self.rect.bottom >= screen_height):
+                self.rect.y = screen_height - 70'''
 
 
 #colors
 WHITE = (255,255,255)
 
 #objects
-ball = py.Rect(screen_width/2-15,screen_height/2-15,30,30)
-player = py.Rect(10,screen_height/2,10,70)
+ball = ball(screen_width/2-15,screen_height/2-15,30,30)
+player = player(10,screen_height/2,10,70)
 opponent = py.Rect(screen_width-20,screen_height/2,10,70)
 
 bg_color = py.Color('grey12')
 light_grey = (200,200,200)
 
-#player
-player_speed = 0
-#ball parameters
-ball_speed_x = 7
-ball_speed_y = 7
+
 
 while True:
 
@@ -53,26 +79,24 @@ while True:
             sys.exit()
         if event.type == py.KEYDOWN:
             if event.key == py.K_DOWN:
-                player_speed += 7
+                player.player_move(7)
             if event.key == py.K_UP:
-                player_speed -= 7
+                player.player_move(-7)
         if event.type == py.KEYUP:
             if event.key == py.K_DOWN:
-                player_speed -= 7
+                player.player_move(-7)
             if event.key == py.K_UP:
-                player_speed += 7
+                player.player_move(7)
 
     screen.fill(bg_color)
 
-    ball_animation()
-    player.y += player_speed
+    ball.ball_update()
+    player.player_update()
 
-    if(player.top <= 0):
-        player.y = 0
-    if(player.bottom >= screen_height):
-        player.y = screen_height - 70
+    if(ball.rect.x > screen_width/2):
+        opponent.y = ball.rect.y
 
-
+    #draw
     py.draw.rect(screen,light_grey,player)
     py.draw.rect(screen,light_grey,opponent)
     py.draw.ellipse(screen,light_grey,ball)
