4e55370396837d016e87346dfbe52a20abe27918
ginestopo <ginestopo@gmail.com>
2021-01-27 21:59:05 +0100

    We got scores!

--- pong.py
+++ pong.py
@@ -1,6 +1,8 @@
 #Pong by Gines Diaz
 import sys
+import os
 import pygame as py
+import random
 
 py.init()
 clock = py.time.Clock()
@@ -9,21 +11,20 @@ clock = py.time.Clock()
 screen_width = 1000
 screen_height = 600
 screen = py.display.set_mode((screen_width,screen_height))
-py.display.set_caption('Game')
+py.display.set_caption('Pong by Gines Diaz')
 
 #functions
 class ball(py.sprite.Sprite):
-    def __init__(self,x,y,x_size,y_size):
+    def __init__(self,x,y,x_size,y_size,ball_speed):
         #ball parameters
-        self.ball_speed_x = 7
-        self.ball_speed_y = 7
+        self.ball_speed_x = ball_speed
+        self.ball_speed_y = ball_speed
 
         py.sprite.Sprite.__init__(self)
 
         self.rect = py.Rect(x,y,x_size,y_size)
 
     def ball_update(self):
-
             self.rect.x += self.ball_speed_x
             self.rect.y += self.ball_speed_y
 
@@ -49,22 +50,48 @@ class player(py.sprite.Sprite):
             self.player_speed += vel
 
     def player_update(self):
-            pos = py.mouse.get_pos()
-            self.rect.center = pos
-            '''self.rect.y += self.player_speed
+            self.rect.y += self.player_speed
             if(self.rect.top <= 0):
                 self.rect.y = 0
             if(self.rect.bottom >= screen_height):
-                self.rect.y = screen_height - 70'''
+                self.rect.y = screen_height - 70
 
+class score(py.sprite.Sprite):
+    def __init__(self,x,y):
+        py.sprite.Sprite.__init__(self)
+        self.images = []
+        self.frame = 0
+
+        for i in range(0,10):
+            img = py.image.load(os.path.join('resources','score0'+str(i)+'.png')).convert()
+            img = py.transform.scale(img, (80, 80))
+            img.convert_alpha()
+            img.set_colorkey(ALPHA)
+            self.images.append(img)
+
+        self.image = self.images[self.frame]
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+    def update(self,score):
+        for i in range (0,10):
+            if(i == score):
+                self.image = self.images[i]
 
 #colors
 WHITE = (255,255,255)
+ALPHA = (0, 0, 0)
 
 #objects
-ball = ball(screen_width/2-15,screen_height/2-15,30,30)
+ball = ball(screen_width/2-15,screen_height/2-15,30,30,7)
 player = player(10,screen_height/2,10,70)
 opponent = py.Rect(screen_width-20,screen_height/2,10,70)
+score = score(screen_width/2 + 100,10)
+score_list = py.sprite.Group()
+score_list.add(score)
+score_number = 0
+screenshake = 0
 
 bg_color = py.Color('grey12')
 light_grey = (200,200,200)
@@ -93,16 +120,37 @@ while True:
     ball.ball_update()
     player.player_update()
 
+
     if(ball.rect.x > screen_width/2):
         opponent.y = ball.rect.y
+
+    #logic
+    if ball.rect.x <= 0:
+        score_number += 1
+        score.update(score_number)
+
+        screenshake = 30
+    if ball.rect.y >= screen_width:
+        print("punto para 2")
+
+
+    if screenshake > 0:
+        screenshake -= 1
+
+    render_offset = [0,0]
+    if screenshake:
+        render_offset[0] = random.randint(0,8) - 4
+        render_offset[1] = random.randint(0,8) - 4
 
     #draw
     py.draw.rect(screen,light_grey,player)
     py.draw.rect(screen,light_grey,opponent)
     py.draw.ellipse(screen,light_grey,ball)
     py.draw.aaline(screen,light_grey,(screen_width/2,0),(screen_width/2,screen_height))
-
+    py.draw.circle(screen,light_grey,(int(screen_width/2),int(screen_height/2)),4)
+    score_list.draw(screen)
 
     #update the window
+    screen.blit(screen, render_offset)
     py.display.flip()
     clock.tick(60)
