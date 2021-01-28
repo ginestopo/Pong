cd197536716a415e598399e15c5cac6f4e410dbb
ginestopo <ginestopo@gmail.com>
2021-01-28 23:18:55 +0100

    score implemented

--- pong.py
+++ pong.py
@@ -86,11 +86,14 @@ ALPHA = (0, 0, 0)
 #objects
 ball = ball(screen_width/2-15,screen_height/2-15,30,30,7)
 player = player(10,screen_height/2,10,70)
-opponent = py.Rect(screen_width-20,screen_height/2,10,70)
+opponent = py.Rect(screen_width-20,screen_height,10,70)
 score_opponent = score(screen_width/2 ,10)
+score_player = score(screen_width/2 - 100 ,10)
 score_list = py.sprite.Group()
 score_list.add(score_opponent)
+score_list.add(score_player)
 score_number_opponent = 0
+score_number_player = 0
 screenshake = 0
 
 bg_color = py.Color('grey12')
@@ -120,7 +123,6 @@ while True:
     ball.ball_update()
     player.player_update()
 
-
     if(ball.rect.x > screen_width/2):
         opponent.y = ball.rect.y
 
@@ -128,10 +130,23 @@ while True:
     if ball.rect.x <= 0:
         score_number_opponent += 1
         score_opponent.update(score_number_opponent)
-
+        score_list.draw(screen)
         screenshake = 30
-    if ball.rect.y >= screen_width:
-        print("punto para 2")
+
+
+        ball.rect.x = screen_width/2 - 15
+        ball.rect.y = screen_height/2 - 15
+
+
+    if ball.rect.right >= screen_width :
+        score_number_player += 1
+        score_player.update(score_number_player)
+        score_list.draw(screen)
+        screenshake = 30
+
+        ball.rect.x = screen_width/2 - 15
+        ball.rect.y = screen_height/2 - 15
+
 
 
     if screenshake > 0:
